bb9c06e3a5cc2530631c6e363940e682d46f24cc
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2024-09-16 22:11:57 -0700

    First item card added

---
+++ src/components/CardList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Add')
+}
+</script>
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Nike Blazer Mid Suede shoes"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="120"
+      :isAdded="false"
+      :is-favorite="false"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
