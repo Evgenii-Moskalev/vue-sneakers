39de12576b2c3a35e384acbdf8fd8f6fbbde30b4
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2024-09-16 19:50:05 -0700

    Project init, shop header completed

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14">
+    <Header />
+    <h1>Main Content</h1>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-300">Shoes Shop</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex item-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/cart.svg" alt="Cart" /> <b>$150</b>
+      </li>
+      <li class="flex item-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/heart.svg" alt="Saved" /> <span>Saved</span>
+      </li>
+      <li class="flex item-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="Profile" /> <span>My Account</span>
+      </li>
+    </ul>
+  </header>
+</template>
