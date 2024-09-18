8fe7eb17b02df855ccbab62b8f6c996ce54e4505
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2024-09-17 20:14:16 -0700

    RightSided Drawer and Cart with ItemsList added

---
+++ src/components/CartItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center border border-slate-200 p-4 rounded-xl gap-4">
+    <img class="w-16 h-16" src="/sneakers/sneakers-2.jpg" alt="Shoe image" />
+
+    <div class="flex flex-col w-full">
+      <p>Nike Air Max 270</p>
+
+      <div class="flex justify-between mt-2">
+        <b>$129</b>
+        <img
+          class="opacity-40 hover:opacity-100 cursor-pointer transition"
+          src="/close.svg"
+          alt="Close"
+        />
+      </div>
+    </div>
+  </div>
+</template>
